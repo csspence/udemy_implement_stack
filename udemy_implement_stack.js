8d465c380a7b297c70775e47f4b5efed99c8207a
Crew Spence <csspence123@gmail.com>
2020-10-14 05:14:37 -0500

    Created function stub for toy problem.

---
+++ udemy_implement_stack.js
@@ -0,0 +1,19 @@
+/*
+Udemy JavaScript Algorithms and Data Structures Masterclass
+Stack Implementation
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
