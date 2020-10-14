09248b3ff8d7a37ecdd017f3bec2759dee4c370c
Crew Spence <csspence123@gmail.com>
2020-10-14 05:36:32 -0500

    Implemented stack.

--- udemy_implement_stack.js
+++ udemy_implement_stack.js
@@ -16,4 +16,31 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
+  push(value) {
+    let node = new Node(value);
+    if(this.size === 0) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let temp = this.first
+      this.first = node;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    let temp = this.first;
+    if(this.size === 0) {
+      return null;
+    } else if(this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+    this.size--;
+    return temp;
+  }
 }
